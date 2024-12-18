f0b5ece02eeb18991260fea569c6e7533969702a
Jhonathan Santos <165796810+Jhots@users.noreply.github.com>
2024-12-18 11:28:55 -0300

    Revert "BUS-11934 feat: Gerar evento de filme buscado e publicar no tópico"

--- src/test/java/com/pagsestagio/movieapi/IntegrationBaseTest.java
+++ src/test/java/com/pagsestagio/movieapi/IntegrationBaseTest.java
@@ -2,7 +2,6 @@ package com.pagsestagio.movieapi;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,5 +9,4 @@ import org.springframework.transaction.annotation.Transactional;
 @ActiveProfiles("dev")
 @AutoConfigureMockMvc
 @Transactional
-@EmbeddedKafka(partitions = 1, topics = { "movie-api.filme-pesquisado" })
 public abstract class IntegrationBaseTest {}
